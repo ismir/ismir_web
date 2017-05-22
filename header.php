794cb6460abb5c49591d1a30bf1494e8951a8ca5
Yann Bayle <bayle.yann@live.fr>
2017-05-22 19:32:17 -0400

    Fixed issue #13 Footer fixes

---
+++ header.php
@@ -0,0 +1,7 @@
+<div id="header">
+<h1>
+<a href="http://ismir.net">
+  <img src="img/ismir_04.png" alt="The International Society of Music Information Retrieval"
+       style="width:300px;margin-left:-48px; margin-bottom:-100px"></a>
+</h1>
+</div>
---
+++ membership.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<?php include 'head.php'; ?>
+</head>
+
+<body>
+<div id="container">
+<?php include 'header.php'; ?>
+<div id="spacer"><img src="img/barstaff.png" alt="staff2circuit" style="width:900px"></div>
+<div id="content-container">
+<?php include 'navigation.php'; ?>
+<div id="content">
+
+<h2>Membership</h2>
+<p>
+As a non-profit organziation with nary a revenue stream, the society relies almost
+entirely on membership dues to cover operational costs.
+</br>
+</p>
+
+<h3>Benefits</h3>
+<p>
+  <ul>
+  <li>Membership rates for conference attendance.</li>
+  <li>Voting rights at the annual business meeting.</li>
+  <li>Spiritual enlightenment and generally good karma.</li>
+  </ul>
+</br>
+</p>
+
+<h3>Becoming a Member</h3>
+<p>
+So you've decided to join the society? That's fantastic news! Follow these
+simple steps to make it official:
+<ol>
+<li>Go to the <a href="https://www.softconf.com/f/ismir-member" target="_blank">membership portal</a></li>
+<li>Choose "For participants: Click HERE to register as a participant to the event."</li>
+<li>Enter your "Contact information"</li>
+<li>Select "Checkout"</li>
+<li>Complete the checkout process with PayPal</li>
+</ol>
+</p>
+<p>
+  And that's it! Once you've completed the process, you will be redirected back
+  to the "softconf-ISMIR" website, showing payment confirmation.
+</p>
+<p>
+  If you happen upon any difficulties or questions, please
+   <a href="mailto:webmaster@ismir.net">contact the webmaster</a>.
+</p>
+
+</div>
+<?php include 'footer.php'; ?>
+</div>
+</div>
+</body>
+</html>

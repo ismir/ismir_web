b33b53c23270f22c3c3b55eb87cf61da7c1425dc
Andy Sarroff <sarroff@cs.dartmouth.edu>
2017-10-06 12:36:20 -0500

    add ghistory of awards

--- conferences.php
+++ conferences.php
@@ -121,6 +121,202 @@
 
 </ul>
 
+<h2>History of Awards</h2>
+<p>Historical information about awards has been collected from the community and may contain inaccuracies. If you'd like to suggest a correction or addition, please file an issue or pull request on <a href="https://github.com/ismir/ismir_web">GitHub</a>.</p>
+<ul>
+<li>
+<b>2016</b>
+<ul>
+  <li>
+  <b>Best paper</b>: Ryan Groves. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/Groves16">Automatic Melodic Reduction Using a Supervised Probabilistic Context-Free Grammar</a>.
+  </li>
+  <li>
+  <b>Best student paper</b>: Maria Panteli, Emmanouil Benetos and Simon Dixon. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/PanteliBD16">Learning a Feature Space for Similarity in World Music</a>.
+  </li>
+  <li>
+  <b>Best oral presentation</b>: Jan Schlüter. <a href="ismir">Learning to Pinpoint Singing Voice from Weakly Labeled Examples</a>.
+  </li>
+  <li>
+  <b>Best poster presentation</b>: Bob L. Sturm. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/Sturm16">Revisiting Priorities: Improving MIR Evaluation Practices</a>.
+  </li>
+  <li>
+  <b>Best reviewer</b>: Francisco Rodríguez-Algarra
+  </li>
+  <li>
+  <b>Other</b>: CogMIR Satellite: Bastiaan van der Weij, University of Amsterdam (Best Paper); Andrew Lambert, City University of London (Best Poster)
+  </li>
+</ul>
+</li>
+<li>
+<b>2015</b>
+<ul>
+  <li>
+  <b>Best paper</b>: Andreas Arzt and Gerhard Widmer. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/ArztW15">Real-time Music Tracking using Multiple Performances as a Reference</a>.
+  </li>
+  <li>
+  <b>Best student paper</b>: Colin Raffel and Daniel Ellis. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/RaffelE15">Large-Scale Content-Based Matching of MIDI and Audio Files</a>.
+  </li>
+  <li>
+  <b>Best oral presentation</b>: Rachel M. Bittner, Justin Salamon, Slim Essid, and Juan P. Bello <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/BittnerSEB15">Melody Extraction by Contour Classification</a>.
+  </li>
+  <li>
+  <b>Best poster presentation</b>: Jonathan Driedger, Thomas Prätzlich, and Meinard Müller <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/DriedgerPM15">Let it Bee – Towards NMF-Inspired Audio Mosaicing</a>.
+  </li>
+  <li>
+  <b>Best reviewer</b>: Blair Kaneshiro; Sankalp Gulati
+  </li>
+</ul>
+</li>
+<li>
+<b>2014</b>
+<ul>
+  <li>
+  <b>Best paper</b>: Zhe Xing, Xinxi Wang, and Ye Wang. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/XingWW14">Enhancing Collaborative Filtering Music Recommendation by Balancing Exploration and Exploitation</a>.
+  </li>
+  <li>
+  <b>Best student paper</b>: Joshua Moore, Thorsten Joachims, and Douglas Turnbull. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/MooreJT14">Taste Space Versus the World: an Embedding Analysis of Listening Habits and Geography</a>.
+  </li>
+  <li>
+  <b>Best oral presentation</b>: Brian McFee and Dan Ellis <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/McFeeE14">Analyzing Song Structure with Spectral Clustering</a>.
+  </li>
+  <li>
+  <b>Best poster presentation</b>: Colin Raffel, Brian McFee, Eric J. Humphrey, Justin Salamon, Oriol Nieto, Dawen Liang, and Daniel P. W. Ellis <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/RaffelMHSNLE14">MIR_EVAL: A Transparent Implementation of Common MIR Metrics</a>.
+  </li>
+  <li>
+  <b>Best reviewer</b>: Julián Urbano, Maarten Grachten, Tom Collins
+  </li>
+</ul>
+</li>
+<li>
+<b>2013</b>
+<ul>
+  <li>
+  <b>Best paper</b>: Same as Best Student Paper award
+  </li>
+  <li>
+    <b>Best student paper</b>:
+    <ul>
+      <li>Srikanth Cherla, Tillman Weyde, Artur Garcez and Marcus Pearce. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/CherlaWGP13">A Distributed Model For Multiple-Viewpoint Melodic Prediction</a>.
+      </li>
+      <li>
+	Vipul Arora and Laxmidhar Behera. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/AroraB13">Semi-Supervised Polyphonic Source Identification Using PLCA Based Graph Clustering</a>.
+      </li>
+      <li>Dawen Liang, Matthew D. Hoffman and Daniel P. W. Ellis <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/LiangHE13">Beta Process Sparse Nonnegative Matrix Factorization for Music</a>.
+      </li>
+    </ul>
+  </li>
+  <li>
+  <b>Best oral presentation</b>: Kazuyoshi Yoshii, Ryota Tomioka, Daichi Mochihashi, and Masataka Goto <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/YoshiiTMG13">Beyond NMF: Time-Domain Audio Source Separation without Phase Reconstruction</a>.
+  </li>
+  <li>
+  <b>Best poster presentation</b>: Harald Grohganz, Michael Clausen, Nanzhu Jiang, and Meinard Müller <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/GrohganzCJM13">Converting Path Structures into Block Structures using Eigenvalue Decompositions of Self-Similarity Matrices</a>.
+  </li>
+  <li>
+  <b>Best reviewer</b>: Emmanouil Benetos; Tom Collins; Michael Scott Cuthbert; Maarten Grachten; Olivier Lartillot; Cynthia Liem; Andrew Robertson; Gabriel Sargent; Andy Sarroff; Jan Schlüter; Bob Sturm
+  </li>
+  <li>
+  <b>Other</b>: Logo competition: Anna Aljanaki, Utrecht University; Olmo Cornelis, University College Ghent
+  </li>
+</ul>
+</li>
+<li>
+<b>2012</b>
+<ul>
+  <li>
+  <b>Best paper</b>: Arthur Flexer, Dominik Schnitzer, and Jan Schlüter: <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/FlexerSS12">A MIREX meta-analysis of hubness in audio music similarity</a>.
+  </li>
+  <li>
+  <b>Best student paper</b>: Véronique Sébastien, Henri Ralambondrainy, Olivier Sébastien, and Noël Conruyt. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/SebastienRSC12">Score Analyzer: Automatically Determining Scores Difficulty Level for Instrumental e-Learning</a>.
+  </li>
+  <li>
+  <b>Best oral presentation</b>: Jean-Julien Aucouturier and Emmanuel Bigand. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/AucouturierB12">Mel Cepstrum & Ann Ova: The Difficult Dialog Between MIR and Music Cognition</a>.
+  </li>
+  <li>
+  <b>Best poster presentation</b>: Pablo Sprechmann, Alexander M. Bronstein, and Guillermo Sapiro. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/SprechmannBS12">Real-time Online Singing Voice Separation from Monaural Recordings Using Robust Low-rank Modeling</a>.
+  </li>
+</ul>
+</li>
+<li>
+<b>2011</b>
+<ul>
+  <li>
+  <b>Best paper</b>: Meinard Müller, Peter Grosche, and Nanzhu Jiang. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/MullerGJ11">A Segment-Based Fitness Measure for Capturing Repetitive Structures of Music Recordings</a>. 
+  </li>
+  <li>
+  <b>Best student paper</b>: Mikael Henaff, Kevin Jarrett, Koray Kavukcuoglu, and Yann LeCun. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/HenaffJKL11">Unsupervised Learning of Sparse Features for Scalable Audio Classification</a>.
+  </li>
+  <li>
+  <b>Best oral presentation</b>: Ryunosuke Daido, Seong-Jun Hahm, Masashi Ito, Shozo Makino and Akinori Ito. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/DaidoHIMI11">A System for Evaluating Singing Enthusiasm for Karaoke</a>.
+  </li>
+</ul>
+</li>
+<li>
+<b>2010</b>
+<ul>
+  <li>
+  <b>Best paper</b>: Ron Weiss and Juan Bello. <a href= "http://dblp.uni-trier.de/rec/html/conf/ismir/WeissB10">Identifying Repeated Patterns in Music Using Sparse Convolutive Non-Negative Matrix Factorization</a>.
+  </li>
+  <li>
+  <b>Best student paper</b>: Hussein Hirjee and Daniel G. Brown. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/HirjeeB10">Solving Misheard Lyric Search Queries Using a Probabilistic Model of Speech Sounds</a>.
+  </li>
+</ul>
+</li>
+<li>
+<b>2009</b>
+<ul>
+  <li>
+  <b>Best paper</b>: Heittola, T., Klapuri, A., and Virtanen, T.. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/HeittolaKV09">Musical Instrument Recognition in Polyphonic Audio Using Source-Filter Model for Sound Separation</a>.
+  </li>
+  <li>
+  <b>Best student paper</b>: Matthew D. Hoffman, David M. Blei, and Perry R. Cook. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/HoffmanBC09">Easy as CBA: A Simple Probabilistic Model for Tagging Music</a>.
+  </li>
+  <li>
+  <b>Best oral presentation</b>: Peter Grosche and Meinard Müller. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/GroscheM09">A mid-level representation for capturing dominant tempo and pulse information in music recordings</a>. 
+  </li>
+</ul>
+</li>
+<li>
+<b>2008</b>
+<ul>
+  <li>
+  <b>Best student paper</b>: P.-A. Manzagol, T. Bertin-Mahieux, and D. Eck. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/ManzagolBE08">On the use of sparse time relative auditory codes for music</a>.
+  </li>
+</ul>
+</li>
+<li>
+<b>2007</b>
+<ul>
+  <li>
+  <b>Best paper</b>: Not awarded
+  </li>
+  <li>
+  <b>Best student paper</b>: Not awarded
+  </li>
+  <li>
+  <b>Best oral presentation</b>: Not awarded
+  </li>
+  <li>
+  <b>Best poster presentation</b>: Not awarded
+  </li>
+</ul>
+</li>
+<li>
+<b>2006</b>
+<ul>
+  <li>
+  <b>Best paper</b>: McKay, C., D. McEnnis, and I. Fujinaga. 2006. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/McKayMF06">"A large publicly accessible prototype audio database for music research</a>.
+  </li>
+</ul>
+</li>
+<li>
+<b>2005</b>
+<ul>
+  <li>
+  <b>Best poster presentation</b>: S. Dixon and G. Widmer. <a href="http://dblp.uni-trier.de/rec/html/conf/ismir/DixonW05">MATCH: A Music Alignment Tool Chest</a>.
+  </li>
+</ul>
+</li>
+</ul>
 
 </div>
 <?php include 'footer.php'; ?>

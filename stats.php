e34d57d74b8dd459c403006651094dd333a73ce5
Andy Sarroff <sarroff@cs.dartmouth.edu>
2017-11-10 10:58:22 -0500

    update 2017 stats

--- stats.php
+++ stats.php
@@ -10,6 +10,7 @@
 <div id="content-container">
 <?php include 'navigation.php'; ?>
 <div id="content">
+
 <h2>ISMIR Paper and Membership Statistics</h2>
 <p>The annual Conference of the <a href="http://www.ismir.net/">International Society for Music Information Retrieval</a> (ISMIR) is the world&#8217;s leading research forum on processing, searching, organizing and accessing music-related data. The ISMIR Conference provides a meeting place for the discussion of MIR-related research. Its main goal is to foster the exchange of ideas across disciplines by bringing together researchers, developers, educators, librarians, students, and professional users.</p>
 <p>ISMIR is a non-profit organization which, among other things, oversees the organization of the ISMIR Conference.</p>
@@ -17,7 +18,7 @@
 <p><a href="http://ismir.net"><img src="img/conference_stats.png" alt="conference_stats"></a></p>
 <p><a href="http://ismir.net"><img src="img/membership_stats.png" alt="membership_stats"></a></p>
 <h2>Impact</h2>
-<p><b>ISMIR</b> is currently the <b>6th ranked publication in the &#8220;Multimedia&#8221; subcategory of &#8220;Engineering and Computer Science&#8221;</b> <a href="http://scholar.google.com/citations?view_op=top_venues&amp;hl=en&amp;vq=eng_multimedia" target="new">(Link.)</a></p>
+<p><b>ISMIR</b> is currently the <b>8th ranked publication in the &#8220;Multimedia&#8221; subcategory of &#8220;Engineering and Computer Science&#8221;</b> <a href="https://scholar.google.com/citations?view_op=top_venues&hl=en&vq=eng_multimedia" target="new">(Link.)</a></p>
 <table border="1" class="dataframe">
   <thead>
     <tr style="text-align: right;">
@@ -30,67 +31,67 @@
   <tbody>
     <tr>
       <td>1</td>
-      <td>IEEE Transactions on Circuits and Systems for Video Technology</td>
-      <td>51</td>
-      <td>70</td>
+      <td>IEEE Transactions on Multimedia</td>
+      <td>58</td>
+      <td>87</td>
     </tr>
     <tr>
       <td>2</td>
-      <td>ACM International Conference on Multimedia</td>
-      <td>45</td>
-      <td>74</td>
+      <td>IEEE Transactions on Circuits and Systems for Video Technology</td>
+      <td>54</td>
+      <td>83</td>
     </tr>
     <tr>
       <td>3</td>
-      <td>IEEE Transactions on Multimedia</td>
-      <td>45</td>
-      <td>58</td>
+      <td>ACM International Conference on Multimedia</td>
+      <td>44</td>
+      <td>67</td>
     </tr>
     <tr>
       <td>4</td>
-      <td>IEEE International Conference on Image Processing (ICIP)</td>
-      <td>38</td>
-      <td>46</td>
+      <td>Multimedia Tools and Applications</td>
+      <td>37</td>
+      <td>49</td>
     </tr>
     <tr>
       <td>5</td>
-      <td>Multimedia Tools and Applications</td>
-      <td>33</td>
-      <td>44</td>
+      <td>Journal of Visual Communication and Image Representation</td>
+      <td>36</td>
+      <td>51</td>
     </tr>
     <tr>
       <td>6</td>
-      <td><strong>International Society for Music Information Retrieval Conference</strong></td>
-      <td>31</td>
+      <td>IEEE International Conference on Image Processing (ICIP)</td>
+      <td>34</td>
       <td>46</td>
     </tr>
     <tr>
       <td>7</td>
       <td>Signal Processing: Image Communication</td>
-      <td>29</td>
-      <td>39</td>
+      <td>31</td>
+      <td>61</td>
     </tr>
     <tr>
       <td>8</td>
-      <td>Journal of Visual Communication and Image Representation</td>
-      <td>28</td>
-      <td>39</td>
+      <td><strong>International Society for Music Information Retrieval Conference</strong></td>
+      <td>29</td>
+      <td>42</td>
     </tr>
     <tr>
       <td>9</td>
-      <td>ACM Multimedia Systems Conference (MMSys)</td>
-      <td>24</td>
-      <td>48</td>
+      <td>ACM International Conference on Multimedia Retrieval</td>
+      <td>29</td>
+      <td>41</td>
     </tr>
     <tr>
       <td>10</td>
-      <td>IEEE International Conference on Multimedia and Expo</td>
-      <td>24</td>
-      <td>28</td>
+      <td>arXiv Multimedia (cs.MM)</td>
+      <td>26</td>
+      <td>38</td>
     </tr>
   </tbody>
 </table><p>&nbsp;</p>
-<p>and the <b>1st ranked in the &#8220;Music &amp; Musicology&#8221; subcategory of &#8220;Humanities, Literature, and Arts&#8221;</b> <a href="http://scholar.google.com/citations?view_op=top_venues&amp;hl=en&amp;vq=hum_musicmusicology&amp;venue=XBwMiZEPey4J.2014" target="new">(Link.)</a></p>
+<p>and the <b>2nd ranked in the &#8220;Music &amp; Musicology&#8221; subcategory of &#8220;Humanities, Literature, and Arts&#8221;</b> <a href="https://scholar.google.com/citations?view_op=top_venues&hl=en&vq=hum_musicmusicology" target="new">(Link.)</a></p>
 <table border="1" class="dataframe">
   <thead>
     <tr style="text-align: right;">
@@ -103,66 +104,67 @@
   <tbody>
     <tr>
       <td>1</td>
-      <td><strong>International Society for Music Information Retrieval Conference</strong></td>
+      <td>Psychology of Music</td>
       <td>31</td>
-      <td>46</td>
+      <td>48</td>
     </tr>
     <tr>
       <td>2</td>
-      <td>Music Perception: An Interdisciplinary Journal</td>
-      <td>27</td>
-      <td>40</td>
+      <td><strong>International Society for Music Information Retrieval Conference</strong></td>
+      <td>29</td>
+      <td>42</td>
     </tr>
     <tr>
       <td>3</td>
-      <td>Psychology of Music</td>
-      <td>22</td>
-      <td>33</td>
+      <td>Music Perception: An Interdisciplinary Journal</td>
+      <td>23</td>
+      <td>34</td>
     </tr>
     <tr>
       <td>4</td>
-      <td>New Interfaces for Musical Expression (NIME)</td>
-      <td>17</td>
-      <td>21</td>
+      <td>Journal of Research in Music Education</td>
+      <td>19</td>
+      <td>26</td>
     </tr>
     <tr>
       <td>5</td>
-      <td>Journal of Music Therapy</td>
-      <td>16</td>
-      <td>23</td>
+      <td>Music Educators Journal</td>
+      <td>18</td>
+      <td>26</td>
     </tr>
     <tr>
       <td>6</td>
-      <td>Journal of Research in Music Education</td>
-      <td>16</td>
-      <td>23</td>
+      <td>Musicae Scientiae</td>
+      <td>18</td>
+      <td>24</td>
     </tr>
     <tr>
       <td>7</td>
-      <td>Musicae Scientiae</td>
-      <td>15</td>
-      <td>29</td>
+      <td>Journal of New Music Research</td>
+      <td>18</td>
+      <td>23</td>
     </tr>
     <tr>
       <td>8</td>
-      <td>Journal of New Music Research</td>
-      <td>15</td>
-      <td>18</td>
+      <td>International Journal of Music Education</td>
+      <td>17</td>
+      <td>24</td>
     </tr>
     <tr>
       <td>9</td>
-      <td>Computer Music Journal</td>
-      <td>14</td>
-      <td>21</td>
+      <td>New Interfaces for Musical Expression (NIME)</td>
+      <td>17</td>
+      <td>22</td>
     </tr>
     <tr>
       <td>10</td>
-      <td>British Journal of Music Education</td>
-      <td>13</td>
-      <td>31</td>
+      <td>Nordic Journal of Music Therapy</td>
+      <td>16</td>
+      <td>20</td>
     </tr>
   </tbody>
 </table>
+
 </div>
 <?php include 'footer.php'; ?>
 </div>

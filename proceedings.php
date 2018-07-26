98082224228dd06788c71a76019c6c63e48aca84
Meinard Müller <meinard.mueller@audiolabs-erlangen.de>
2018-07-26 13:13:36 +0200

    Start with tutorials and proceedings

---
+++ proceedings.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<?php include 'head.php'; ?>
+</head>
+
+<body>
+<div id="container">
+<?php include 'header.php'; ?>
+<div id="spacer"><img src="img/barstaff.png" alt="staff2circuit" style="width:900px"></div>
+<div id="content-container">
+<?php include 'navigation.php'; ?>
+<div id="content">
+  <h2>Proceedings of ISMIR conferences</h2>
+
+  <p>Text.</p>
+  
+<p class="clear" />
+<a href="proceedings/2015_ISMIR_Proceedings.pdf"><img src="proceedings/2015_ISMIR_Proceedings_Cover.jpg" alt="2015_ISMIR_Proceedings_Cover.jpg" width="100" class="left" style="border:1px solid black"></a>
+<p>Meinard Müller, Frans Wiering<br>
+<a href="http://ismir2015.uma.es/index.html" target="_blank">Proceedings of the International Society for Music Information Retrieval Conference (ISMIR), 2015</a><br>
+ISBN 987-84-606-8853-2<br>
+<a href="proceedings/2015_ISMIR_Proceedings.pdf">PDF</a>, <a href="https://dblp1.uni-trier.de/db/conf/ismir/ismir2015.html" target="_blank">DBLP</a><br></p>
+<p class="clear" />
+ 
+
+</div>
+<?php include 'footer.php'; ?>
+</div>
+</div>
+</body>
+</html>
---
+++ tutorials.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<?php include 'head.php'; ?>
+</head>
+
+<body>
+<div id="container">
+<?php include 'header.php'; ?>
+<div id="spacer"><img src="img/barstaff.png" alt="staff2circuit" style="width:900px"></div>
+<div id="content-container">
+<?php include 'navigation.php'; ?>
+<div id="content">
+  <h2>Tutorials at ISMIR conferences</h2>
+
+  <p>Text.</p>
+
+  <p><a href="#year2018">2018</a> <a href="#year2017">2017</a> <a href="#year2016">2016</a> <a href="#year2015">2015</a></p>
+  
+  <h3 id="#year2018">2018</h3>
+  <ul>
+    <li>
+	   Brian McFee, Thor Kell<br/> 
+	   <b>Open Source and Reproducible MIR Research</b><br/>  
+	   <a href="http://ismir2018.ircam.fr/pages/events-tutorial-14.html" target="_blank">Tutorial of the International Society for Music Information Retrieval Conference (ISMIR), 2018</a>
+	</li>
+    <li>
+	   Xavier Serra, Martin Clayton, Barış Bozkurt<br/> 
+	   <b>Computational Approaches for Analysis of Non-Western Music Traditions</b><br/>  
+	   <a href="http://ismir2018.ircam.fr/pages/events-tutorial-09.html" target="_blank">Tutorial of the International Society for Music Information Retrieval Conference (ISMIR), 2018</a>
+	</li>	
+  </ul>
+
+
+</div>
+<?php include 'footer.php'; ?>
+</div>
+</div>
+</body>
+</html>

3cbca91c60ca16a89dd4f7e6d6b3aeb79f66c71a
Frank Zalkow <frank.zalkow@audiolabs-erlangen.de>
2018-07-27 10:19:20 +0200

    left/right align pics and text for proceedings

--- proceedings.php
+++ proceedings.php
@@ -13,15 +13,20 @@
   <h2>Proceedings of ISMIR conferences</h2>
 
   <p>Text.</p>
-  
-<p class="clear" />
-<a href="proceedings/2015_ISMIR_Proceedings.pdf"><img src="proceedings/2015_ISMIR_Proceedings_Cover.jpg" alt="2015_ISMIR_Proceedings_Cover.jpg" width="100" class="left" style="border:1px solid black"></a>
-<p>Meinard Müller, Frans Wiering<br>
-<a href="http://ismir2015.uma.es/index.html" target="_blank">Proceedings of the International Society for Music Information Retrieval Conference (ISMIR), 2015</a><br>
-ISBN 987-84-606-8853-2<br>
-<a href="proceedings/2015_ISMIR_Proceedings.pdf">PDF</a>, <a href="https://dblp1.uni-trier.de/db/conf/ismir/ismir2015.html" target="_blank">DBLP</a><br></p>
-<p class="clear" />
- 
+
+<div class="proceedings-container">
+  <div class="proceedings-left">
+    <a href="proceedings/2015_ISMIR_Proceedings.pdf">
+      <img src="proceedings/2015_ISMIR_Proceedings_Cover.jpg" alt="2015_ISMIR_Proceedings_Cover.jpg" width="100" class="left" style="border:1px solid black">
+    </a>
+  </div>
+  <div class="proceedings-right">
+    <p>Meinard Müller, Frans Wiering<br>
+    <a href="http://ismir2015.uma.es/index.html" target="_blank">Proceedings of the International Society for Music Information Retrieval Conference (ISMIR), 2015</a><br>
+    ISBN 987-84-606-8853-2<br>
+    <a href="proceedings/2015_ISMIR_Proceedings.pdf">PDF</a>, <a href="https://dblp1.uni-trier.de/db/conf/ismir/ismir2015.html" target="_blank">DBLP</a><br></p>
+  </div>
+</div>
 
 </div>
 <?php include 'footer.php'; ?>
